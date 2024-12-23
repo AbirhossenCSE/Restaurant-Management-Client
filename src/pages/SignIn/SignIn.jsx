ab029f55b388dab1655386bd7fdd12b52a0282c6
Abir Hossen <abir.cse4@gmail.com>
2024-12-23 19:51:48 +0600

    register & signin

---
+++ src/pages/SignIn/SignIn.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import useAuth from '../../hooks/useAuth';
+
+const SignIn = () => {
+    const { signInUser } = useAuth();
+
+    // const location = useLocation();
+    // const navigate = useNavigate();
+    // const from = location.state || '/';
+
+    const handleSignIn = e => {
+        e.preventDefault();
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+
+        // signin user
+        signInUser(email, password)
+            .then(result => {
+                console.log("signin", result.user.email);
+                // const user = { email: email }
+                // axios.post('http://localhost:5000/jwt', user, { withCredentials: true })
+                //     .then(res => {
+                //         console.log(res.data);
+                //     })
+                // // navigate(from);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+
+    }
+
+    return (
+        <div className="hero bg-base-100 min-h-screen">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <div className="text-center lg:text-left w-96">
+                    {/* <Lottie animationData={signinLottile}></Lottie> */}
+                </div>
+                <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                    <h1 className="mx-auto mt-5 text-5xl font-bold">Sign-In now!</h1>
+                    <form onSubmit={handleSignIn} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" name='email' placeholder="email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" name='password' placeholder="password" className="input input-bordered" required />
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Login</button>
+                        </div>
+                    </form>
+                    {/* <SocialLogin></SocialLogin> */}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SignIn;
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,13 +1,33 @@
 import {
     createBrowserRouter,
-  } from "react-router-dom";
+} from "react-router-dom";
+import MainLayout from "../layout/MainLayout";
+import Home from "../pages/Home/Home";
+import Register from "../pages/Register/Register";
+import SignIn from "../pages/SignIn/SignIn";
 
-  const router = createBrowserRouter([
+const router = createBrowserRouter([
     {
-      path: "/",
-      element: <div>Hello world!</div>,
+        path: "/",
+        element: <MainLayout></MainLayout>,
+        errorElement: <h2>Page not found</h2>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/register',
+                element: <Register></Register>,
+            },
+            {
+                path: '/signin',
+                element: <SignIn></SignIn>,
+            },
+
+        ]
     },
-  ]);
+]);
 
 
-  export default router;
+export default router;
