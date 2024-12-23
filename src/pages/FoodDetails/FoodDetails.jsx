07048fd684690a59e7abf26666f1e251617975a5
Abir Hossen <abir.cse4@gmail.com>
2024-12-24 01:47:30 +0600

    food details

--- src/pages/FoodDetails/FoodDetails.jsx
+++ src/pages/FoodDetails/FoodDetails.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { FaDollarSign } from 'react-icons/fa';
 import { useLoaderData } from 'react-router-dom';
 
 const FoodDetails = () => {
     const { _id, foodName, foodImage, foodCategory, price, quantity, foodOrigin, description } = useLoaderData();
     return (
         <div>
-            <div className="card bg-base-100 w-4/6 mx-auto shadow-xl">
+            <div className="card bg-base-200 w-4/6 mx-auto my-16 shadow-xl">
                 <figure>
                     <img
                         src={foodImage}
@@ -17,11 +18,12 @@ const FoodDetails = () => {
                         <div className="badge badge-secondary">NEW</div>
                     </h2>
                     <div className='p-4 '>
-                        <p>Food Category: {foodCategory}</p>
-                        <p>Food Price: {price}</p>
-                        <p>Available: {quantity}</p>
-                        <p>Food Origin: {foodOrigin}</p>
-                        <p>Description: {description}</p>
+                        <p><strong>Food Category:</strong> {foodCategory}</p>
+                        <p className='flex items-center'>
+                            <strong>Food Price:</strong> <FaDollarSign />{price}</p>
+                        <p><strong>Available:</strong> {quantity}</p>
+                        <p><strong>Food Origin:</strong> {foodOrigin}</p>
+                        <p><strong>Description:</strong> {description}</p>
                     </div>
                     <div className="card-actions justify-end">
                         <div className="badge badge-outline">Fashion</div>
