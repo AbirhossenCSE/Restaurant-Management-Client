ab22efd55d9445858a64ca44095a1d469d4fae32
Abir Hossen <abir.cse4@gmail.com>
2025-05-11 14:26:08 +0600

    done

--- src/pages/ErrorPage/Error.jsx
+++ src/pages/ErrorPage/Error.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Error = () => {
     return (
         <div className='w-10/12 mx-auto'>
             <img className='mx-auto' src="https://i.ibb.co.com/NrGjttC/404.gif" alt="" />
+            <Link to='/' className='btn btn-neutral flex items-center justify-center w-1/5 mx-auto'>Go Back to Home</Link>
         </div>
     );
 };
